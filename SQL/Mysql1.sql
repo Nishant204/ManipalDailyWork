2d633feb5383173afc71e4ddbf2ec7d79313a752
Nishant <nishantraj204@gmail.com>
2023-01-21 22:06:30 +0530

    SQL- query

---
+++ SQL/Mysql1.sql
@@ -0,0 +1,4 @@
+
+
+create table customer(customerid int auto_increment primary key, customerName varchar(100), customerEmail varchar(100), Customerpassword int , customerPhone Bigint, customerCardno Bigint); 
+ create table Account(account_number Bigint auto_increment primary key, account_balance float, Account_type varchar(100), bankid int , foreign key());
